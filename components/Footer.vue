d2ee5aea954f64262a2e83745d0907f4cc1dd783
некит <nickitos042004@gmail.com>
2024-10-11 00:27:35 +0400

    init comit

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-blue-400 footer">
+        
+    </div>
+    
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+    .footer{
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 10;
+        padding: 20px;
+    }
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-blue-400">
+    <p>а это хедер</p>
+  </div>
+  
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
